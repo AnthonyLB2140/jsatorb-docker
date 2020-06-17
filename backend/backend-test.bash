f97a6a175a76e837d9aac1e694af96f975bfbb11
Olivier PODEVIN <olivier.podevin@c-s.fr>
2020-06-17 20:35:00 +0200

    Add expected backend test script server response

--- backend/backend-test.bash
+++ backend/backend-test.bash
@@ -8,3 +8,6 @@
 # running.
 # It can be used either in the backend Docker container or in the Docker host.
 curl -H "content-type: application/json" -X GET http://localhost:8000/missiondata/Mission-Test-01
+
+# Expected server JSON response:
+# {"status": "FAIL", "message": "File /root/JSatOrb/mission-data/Mission-Test-01.jso doesn't exists !", "data": null}
